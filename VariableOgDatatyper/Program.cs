ae66b6ff869b9de839bd9aeb20655af37d6c6952
Jonas Offersen <72074478+zelanard@users.noreply.github.com>
2023-08-10 10:02:58 +0200

    Add project files.

---
+++ VariableOgDatatyper/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace VariableOgDatatyper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 16;
+            double width = 5.1;
+            int area = length * width;
+            /** Visual Studios compiler vil ikke oversætte din kode, men hvorfor?
+             * double x int = double.
+             * int kan kun indeholde heltal hvorfor du ikke kan assign en double til en int. 
+             * Jeg er nødt til at caste datatyppen for at det skal kunne virke.
+             * Dette kan jeg gøre enten med (int) eller Convert.ToInt32(width).
+            */
+
+            double d = width * length;
+            /** Her vil Visual Studio gerne oversætte din kode, men hvorfor nu det?
+             * double x int = double.
+             */
+            Console.WriteLine(area);
+        }
+    }
+}
